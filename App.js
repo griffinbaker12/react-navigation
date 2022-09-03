d35be6cf87ee14ab9d180b60beb1c952af25cf8e
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2022-09-02 19:49:36 -0500

    proper layout for app: stack => tab => stack

--- App.js
+++ App.js
@@ -1,46 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+import { getFocusedRouteNameFromRoute, NavigationContainer, useFocusEffect } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from "@react-navigation/stack";
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const HomeScreen = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-        <Button title="Checkout Details" onPress={() => navigation.navigate("Details")}/>
-        <StatusBar style="auto" />
-      </View>
-    )
-}
-
-const DetailsScreen = () => {
-    return (
-        <View style={styles.container}>
-        <Text>This is the details screen!</Text>
-        // push another details screen onto the stack
-        <Button title="Checkout Details" onPress={() => navigation.push("Details")}/>
-        <StatusBar style="auto" />
-      </View>
-    )
-}
-
-export default function App() {
+function HomeScreen() {
   return (
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen name="Home" component={HomeScreen} / >
-              <Stack.Screen name="Details" component={DetailsScreen} / >
-          </Stack.Navigator>
-      </NavigationContainer>
+      
+        <Tab.Navigator>
+            <Tab.Screen name="Test1" component={Test1Screen} />
+            <Tab.Screen name="Test2" component={Test2Screen}/>
+        </Tab.Navigator>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Test1Screen = ({navigation}) => {
+    const handlePress = () => navigation.push("Settings")
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Test 1</Text>
+        <Button title='Go to Community Page' onPress={handlePress}/>
+    </View>
+        )
+}
+
+function Random1() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Random 1</Text>
+    </View>
+        )
+}
+
+function Random2() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Random 1</Text>
+    </View>
+        )
+}
+
+function SettingsBottomTab({route}) {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Random1" component={Random1}/>
+            <Tab.Screen name="Random2" component={Random2}/>
+        </Tab.Navigator>
+        )
+}
+
+function Test2Screen({navigation}) {
+    return (
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title='Go to Community Page' onPress={() => navigation.navigate("Settings")}/>
+        <Text>Test 2</Text>
+    </View>
+        )
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Settings" component={SettingsBottomTab}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
